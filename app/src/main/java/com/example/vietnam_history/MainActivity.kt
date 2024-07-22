43900f5f0cc216be79bdcbe4c6a85c4b7b59302e
Mai Tuan Trung <challenger@Mais-MacBook-Pro.local>
2024-07-22 19:56:33 +0700

    nearly complete

--- app/src/main/java/com/example/vietnam_history/MainActivity.kt
+++ app/src/main/java/com/example/vietnam_history/MainActivity.kt
@@ -4,18 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
-import androidx.compose.foundation.layout.calculateEndPadding
-import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
@@ -25,7 +23,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,6 +31,15 @@ import com.example.vietnam_history.data.Datasource
 import com.example.vietnam_history.model.Event
 import com.example.vietnam_history.ui.theme.Vietnam_historyTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.dimensionResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,39 +60,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun HistoryApp() {
-    val layoutDirection = LocalLayoutDirection.current
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .statusBarsPadding()
-            .padding(
-                start = WindowInsets.safeDrawing.asPaddingValues()
-                    .calculateStartPadding(layoutDirection),
-                end = WindowInsets.safeDrawing.asPaddingValues()
-                    .calculateEndPadding(layoutDirection),
-            ),
-    ) {
-        EventList(
-            eventList = Datasource().loadEvents(),
-        )
-    }
-}
+    Scaffold(
+        topBar = {
+            TopAppBar()
+        }
+    ) { paddingValues ->
+        LazyColumn(contentPadding = paddingValues) {
+            items(Datasource().loadEvents()) { event ->
 
-@Composable
-fun EventList(eventList: List<Event>, modifier: Modifier = Modifier) {
-    LazyColumn(modifier = modifier) {
-        items(eventList) { event ->
-            EventCard(
-                event = event,
-                modifier = Modifier.padding(8.dp)
-            )
+                var isExpanded by remember { mutableStateOf(false) }
+
+                EventCard(
+                    event = event,
+                    expanded = isExpanded,
+                    onClick = { isExpanded = !isExpanded },
+                    modifier = Modifier.padding(dimensionResource(R.dimen.padding_small))
+                )
+            }
         }
     }
 }
 
+
 @Composable
-fun EventCard(event: Event, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
+fun EventCard(
+    event: Event,
+    modifier: Modifier = Modifier,
+    expanded: Boolean,
+    onClick: () -> Unit
+) {
+    Card(modifier = modifier.clickable{ onClick() }) {
+
         Column {
             Image(
                 painter = painterResource(event.imageResourceId),
@@ -101,7 +105,55 @@ fun EventCard(event: Event, modifier: Modifier = Modifier) {
                 modifier = Modifier.padding(16.dp),
                 style = MaterialTheme.typography.headlineSmall
             )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            if (expanded) {
+                EventInfo(
+                    event.descriptionResourceId,
+                    modifier = Modifier.padding(
+                        start = dimensionResource(R.dimen.padding_medium),
+                        top = dimensionResource(R.dimen.padding_small),
+                        end = dimensionResource(R.dimen.padding_medium),
+                        bottom = dimensionResource(R.dimen.padding_medium)
+                    )
+                )
+            }
         }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
+
+
+@Composable
+fun EventInfo(
+    @StringRes Info: Int,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ){
+        Text(
+            text = stringResource(Info),
+            style = MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
